fbd31605e30f512440d3ab7110743675ba7dd54e
japesjake <japesjake@gmail.com>
2022-06-07 11:35:43 -0400

    testing

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-import Wedding
-import Table
+from Wedding import *
+from Table import *
 from Person import *
 import csv
 
+while True:
+    num = input("Please enter the maximum number of people per table: ")
+    num = int(num)
+    if isinstance(num, int): break
+while True:
+    start = input("Firstly, open up 'input.csv' in the program directory and input the name, age and spouse (if they have one) of all the guests. Please refer to 'input_example.csv' for an example input. Once you entered all the infomation into 'input.csv' type 'start' and press enter: ")
+    if start == "start": break
 
+wedding = Wedding()
 
 csv_file = open('input.csv', 'r')
 csv_reader = csv.reader(csv_file)
 for name, age, spouse in csv_reader:
     if name == "NAME": continue
     print(name, age, spouse)
-        Person(name, age, spouse)
+    wedding.addPerson(Person(name, age, spouse))
