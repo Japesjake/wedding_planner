da1218735c0bf008452c9adfbe209e43092ae2e1
japesjake <japesjake@gmail.com>
2022-06-26 15:59:47 -0400

    reorganized classes into folers.

---
+++ person/Person.py
@@ -0,0 +1,5 @@
+class Person:
+    def __init__(self, name, age, spouse):
+        self.name = name
+        self.age = age
+        self.spouse = spouse
---
+++ run.py
@@ -0,0 +1,15 @@
+from wedding.Wedding import *
+
+wedding = Wedding()
+
+while True:
+    wedding.max_seats = wedding.query_max_seats()
+    wedding.start()
+    wedding.open_csv()
+    wedding.create_tables()
+    wedding.match_couples()
+    wedding.assign_couples_to_tables()
+    wedding.assign_singles_to_tables()
+
+    wedding.test_tables()
+    wedding.test()
