5c50c6da81d2bc86406e9c6dbe40599f2f6aaa85
japesjake <japesjake@gmail.com>
2022-06-26 02:37:21 -0400

    fixed bug where unassigned_people was edited during iteration leading to early termination of the loop.

--- Wedding.py
+++ Wedding.py
@@ -1,5 +1,5 @@
 import csv
-import math
+import random
 from Person import *
 from Table import *
 
@@ -10,10 +10,6 @@ class Wedding:
         self.tables = []
     def add_table(self, table):
         self.tables.append(table)
-    def remove_table(self, table):
-        self.tables.remove(table)
-    def couple_table(self, table):
-        self.coupled_tables.append(table)
     def add_person(self, person):
         self.unassigned_people.append(person)
     def remove_person(self, person):
@@ -23,21 +19,22 @@ class Wedding:
     def remove_couple(self, couple):
         self.couples.remove(couple)
 
-    # asks for max seats per table.
+    # gets user input for max seats per table.
     def query_max_seats(self):
         while True:
-            # seats = input("Please enter the maximum number of people per table: ")
-            seats = 6#
+            # seats = input("Please enter the maximum number of seats per table: ")
+            seats = 7
 
             try: seats = int(seats)
-            except: print("please enter an integer")
+            except: print("please enter an integer.")
             if isinstance(seats, int): return seats
 
     def start(self):
         while True:
             # start = input("Open up 'input.csv' in the program directory and input the name, age and spouse (if they have one) of all the guests. Please refer to 'input_example.csv' for an example input. Once you entered all the infomation into 'input.csv' type 'start' and press enter: ")
-            start = "start"#
+            start = "start"
             if start == "start": return None
+    
     def open_csv(self):
         csv_file = open('input.csv', 'r')
         csv_reader = csv.reader(csv_file)
@@ -45,53 +42,68 @@ class Wedding:
             if name == "NAME": continue
             self.add_person(Person(name, age, spouse))
 
-
     def round_up(self, numerator, divisor):
         return int((numerator // divisor) + (numerator % divisor > 0))
-
-
 
     def create_tables(self):
         num_people = len(self.unassigned_people)
         tables = self.round_up(num_people, self.max_seats)
-        for i in range(tables):
-            table = Table(self.max_seats, i)
-            self.add_table(table)
+        broken = False
+        while not broken:
+            people = 0
+            self.tables = []
+            for i in range(1, tables + 1):
+                int = random.randint(self.max_seats - 2, self.max_seats)
+                table = Table(int, i)
+                self.add_table(table)
+                people += int
+                if people == len(self.unassigned_people):
+                    broken = True
+                    break
 
     def match_couples(self):
+        people_coupled = []
         for person1 in self.unassigned_people:
+            people_coupled.append(person1)
             for person2 in self.unassigned_people:
-                if person1.name == person2.spouse:
+                if person1.name == person2.spouse and person2 not in people_coupled:
                     self.add_couple((person1, person2))
-    def is_even(self, number):
-        if number % 2 == 0: return True
 
     def assign_couples_to_tables(self):
-        even = self.is_even(self.max_seats)
-        for couple in self.couples:
-            for person in couple:
-                for table in self.tables:
+        for table in self.tables:
+            broken = False
+            for couple in self.couples:
+                i = 0
+                if broken: break
+                for person in couple:
+                    i += 1
                     if person in self.unassigned_people:
-                        if even and table.max_seats >= len(table.people):
-                            self.remove_person(person)
-                            table.add_person(person)
-                        elif not even and table.max_seats >= len(table.people) + 2:
-                            self.remove_person(person)
-                            table.add_person(person)
-                            
-                            
-
-
+                        self.remove_person(person)
+                        table.add_person(person)
+                        # print(person.name, ":", "id = ", table.id, "couple: = ", i)
+                    if table.max_seats <= len(table.people) + 1 and i == 2:
+                        broken = True
+                        if broken: break
+                    
+    
     def assign_singles_to_tables(self):
-        for person in self.unassigned_people:
-            for table in self.tables:
+        for table in self.tables:
+            people = list(self.unassigned_people)
+            for person in people:
                 if table.max_seats > len(table.people):
                     if person in self.unassigned_people:
                         self.remove_person(person)
                         table.add_person(person)
 
-
-    def test(self):
+    def test_tables(self):
         for table in self.tables:
+            print("table" ,table.id, "has", table.max_seats, "seats")
+    
+    def test(self):
+        print()
+        for table in self.tables:
+            print()
+            print("table", table.id)
             for person in table.people:
-                print(person.name, ":", table.id)
+                print(person.name)
+
--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from Wedding import *
 
 wedding = Wedding()
 
-
-wedding.start()
 wedding.max_seats = wedding.query_max_seats()
+wedding.start()
 wedding.open_csv()
 wedding.create_tables()
 wedding.match_couples()
 wedding.assign_couples_to_tables()
 wedding.assign_singles_to_tables()
 
+wedding.test_tables()
 wedding.test()
 # to do #
 
-# add more entries to input.csv file
+# add web compatibility
