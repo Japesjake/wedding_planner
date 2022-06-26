b6afb34f54bb81ad1f2ff3b44d10e7d15bc67067
japesjake <japesjake@gmail.com>
2022-06-26 16:21:15 -0400

    organized class files into folders.

---
+++ table/Table.py
@@ -0,0 +1,13 @@
+# from wedding.Wedding import *
+
+class Table():
+    def __init__(self, max_seats, id):
+        self.max_seats = max_seats
+        self.people = []
+        self.id = id
+        self.open = False
+    def add_person(self, person):
+        self.people.append(person)
+    def is_full(self, table):
+        if len(table.people) >= self.max_seats: return True
+        return False
