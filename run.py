c7b1195658ee52d604eced192efbf4295995650d
JP Jaquith <japesjake@gmail.com>
2022-07-02 12:37:49 -0400

    Added a conditional that deals with non-positive integers.

--- run.py
+++ run.py
@@ -4,12 +4,13 @@ wedding = Wedding()
 
 while True:
     wedding.max_seats = wedding.query_max_seats()
-    wedding.start()
-    wedding.open_csv()
-    wedding.create_tables()
-    wedding.match_couples()
-    wedding.assign_couples_to_tables()
-    wedding.assign_singles_to_tables()
+    if wedding.max_seats != 1:
+        wedding.start()
+        wedding.open_csv()
+        wedding.create_tables()
+        wedding.match_couples()
+        wedding.assign_couples_to_tables()
+        wedding.assign_singles_to_tables()
 
-    wedding.test_tables()
-    wedding.test()
+        wedding.test_tables()
+        wedding.test()
